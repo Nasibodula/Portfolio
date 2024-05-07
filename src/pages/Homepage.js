0958dc158e629ef6f14ad24d57f9cc486ff2dbc6
Nasibo Dula Chudo <119130081+Nasibodula@users.noreply.github.com>
2024-05-07 18:45:53 +0300

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Hero from '../components/Hero.jsx';
+import Hero from '../components/hero.jsx';
 import About from '../components/About.jsx';
 import Achievements from '../components/Achievements.jsx';
 import Footer from '../components/Footer.jsx';
 import Navbar from '../components/Navbar.jsx';
 import Projects from '../components/Project.jsx';
-import Contact from '../components/Contact.jsx';
+import Contact from '../components/contact.jsx';
 
 export default function Homepage() {
   return (
